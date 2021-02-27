46caf57256a9e7135f05c46bbe621e285e051a9f
yong lu <yonglu777@gmail.com>
2021-02-27 00:21:18 -0800

    Add project files.

---
+++ HireTroops/HiringTroops.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+
+
+namespace HireTroops
+{
+    internal class HiringTroops : CampaignBehaviorBase
+    {
+        public override void RegisterEvents() {
+            CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener((object)this, new Action<CampaignGameStarter>(this.AddMenuItem));
+        }
+
+        private void AddMenuItem(CampaignGameStarter campaignGameStarter) {
+            campaignGameStarter.AddGameMenuOption("town", "hire_troops", "Hire Troops", (GameMenuOption.OnConditionDelegate)(args =>
+           {
+               args.Tooltip = new TextObject("Hire Troops");
+               args.IsEnabled = true;
+               args.optionLeaveType = GameMenuOption.LeaveType.Continue;
+               return true;
+                
+           }), (GameMenuOption.OnConsequenceDelegate)(args => InformationManager.DisplayMessage(new InformationMessage("Nice!"))), index: 1);
+        }
+        public override void SyncData(IDataStore dataStore)
+        {
+        }
+
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace HireTroops
+{
+    public class SubModule : MBSubModuleBase
+    {
+        private static readonly List<Action> ActionsToExecuteNextTick = new List<Action>();
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot() => base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (!(game.GameType is Campaign))
+                return;
+            ((CampaignGameStarter)gameStarterObject).AddBehavior((CampaignBehaviorBase)new SmugglerBehavior());
+        }
+
+        public static void ExecuteActionOnNextTick(Action action)
+        {
+            if (action == null)
+                return;
+            SubModule.ActionsToExecuteNextTick.Add(action);
+        }
+
+        protected override void OnApplicationTick(float dt)
+        {
+            base.OnApplicationTick(dt);
+            foreach (Action action in SubModule.ActionsToExecuteNextTick)
+                action();
+            SubModule.ActionsToExecuteNextTick.Clear();
+        }
+
+        protected override void OnSubModuleLoad() => base.OnSubModuleLoad();
+    }
+}
